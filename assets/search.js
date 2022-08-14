38405c6ad127359839282cf8a381483530abe58d
Cyprian Gomes <cypriangomes@Cyprians-MacBook-Air.local>
2022-08-14 14:19:22 +0100

    added currency list on the dropdown

--- assets/search.js
+++ assets/search.js
@@ -171,4 +171,25 @@ $('.ui.dropdown')
   .dropdown();
 
 
+//currency coverter API
 
+  var fromCurrencyItem = document.getElementsByClassName('item');
+  var fromCurrencyMenu = document.getElementsByClassName('menu');
+  var currencyApiKey = "2af4ef798a01b92fe52e4860";
+  
+  var fromCurrency = ['AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BRL','BSD','BTN','BWP','BYN','BZD','CAD','CDF','CHF','CLP','CNY','COP','CRC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','FOK','GBP','GEL','GGP','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','IMP','INR','IQD','IRR','ISK','JEP','JMD','JOD','JPY','KES','KGS','KHR','KID','KMF','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRU','MUR','MVR','MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLE','SOS','SRD','SSP','STN','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TVD','TWD','TZS','UAH','UGX','USD','UYU','UZS','VES','VND','VUV','WST','XAF','XCD','XDR','XOF','XPF','YER','ZAR','ZMW','ZWL'];
+  
+  var fromCurrOption = "";
+  
+  for (var i = 0; i < fromCurrency.length; i++) {
+      console.log(fromCurrency[i]);
+  
+  
+  fromCurrOption += '<div class ="item" data-value="'+ fromCurrency[i] + '">' + fromCurrency[i] + "</div>;"
+  }
+  fromCurrencyItem.innerhtml = fromCurrOption;
+  console.log(fromCurrencyItem.innerhtml);
+  fromCurrencyMenu.innerHTML = fromCurrOption;
+  
+  //https://v6.exchangerate-api.com/v6/2af4ef798a01b92fe52e4860/pair/EUR/GBP
+  
